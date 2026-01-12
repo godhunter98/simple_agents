b876ebd5693260082acc356387f846ac2fdef502
lazy programmer <lazy.programmerp37@gmail.com>
2026-01-13 02:44:40 +0530

    addded support for reading executions from the db based on uuid

--- infrastructure/db.py
+++ infrastructure/db.py
@@ -1,7 +1,9 @@
+from typing import List
 import os
-import json
 import psycopg
+import psycopg.rows
 from domain.models import AgentExecution, ToolCall
+import UUID
 
 def get_connection():
     return psycopg.connect(
@@ -67,5 +69,93 @@ def persist_execution(execution: AgentExecution) -> None:
         conn.rollback()
         raise e
     finally:
+        conn.close()      
+
+def get_tool_calls_for_execution(execution_id:UUID) -> List[ToolCall]:
+    conn = get_connection()
+
+    try:
+        with conn.cursor() as cursor:
+
+            cursor.execute(
+                '''
+                SELECT 
+                    id,
+                    execution_id,
+                    tool_name,
+                    arguments,
+                    call_order,
+                    created_at
+                    FROM tool_calls WHERE execution_id = %s
+                    ORDER BY call_order ASC
+                ''',
+                (execution_id)
+            )
+
+            rows = cursor.fetchall()
+
+            if rows is None:
+                return []
+
+            tool_calls = []
+
+            for row in rows:
+                tool_calls.append(
+                    ToolCall(
+                        id=row["id"],
+                        execution_id=row["execution_id"],
+                        tool_name=row["tool_name"],
+                        arguments=row["arguments"],
+                        call_order=row["call_order"],
+                        created_at=row["created_at"],
+                    )
+                )
+
+            return tool_calls
+
+    finally:
         conn.close()
-        
+
+def get_execution_by_id(execution_id:UUID) -> AgentExecution | None:
+    conn = get_connection()
+
+    try:
+        with conn.cursor() as cursor:
+
+            cursor.execute(
+                '''
+                SELECT 
+                    id, 
+                    query, 
+                    response, 
+                    agent_name, 
+                    model, 
+                    created_at
+                FROM agent_executions WHERE ID = %s
+                ''',
+                (execution_id)
+            )
+
+            row = cursor.fetchone()         
+
+            if row is None:
+                return None
+            
+            execution = AgentExecution(
+                id=row["id"],
+                query=row["query"],
+                response=row["response"],
+                agent_name=row["agent_name"],
+                model=row["model"],
+                created_at=row["created_at"],
+                tool_calls=[],
+            )
+
+            execution.tool_calls = get_tool_calls_for_execution(execution.id)
+
+            return execution
+
+    finally:
+        conn.close()
+
+print(get_execution_by_id(UUID('7ee90d67-43bd-4ae4-86b8-04fd3f42958d')))
